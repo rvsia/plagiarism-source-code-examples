bd444df729e80b5e8d26fbab26d259314365b481
Richard Všianský <r.vsia@seznam.cz>
2019-01-21 21:55:46 +0100

    java example

---
+++ Calculator.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+// Calculator  class
+public class Calculator {
+	// History of results
+	private List<Float> history = new ArrayList<Float>();
+	
+	// Constructor for loading results from file
+	public Calculator(String fileName) {
+		this.loadHistoryFromFile(fileName);
+		this.print("Calculator was created");
+	}
+	
+	// Prints text with current date
+	public void print(String text) {
+		java.util.Date date = new java.util.Date();
+	    System.out.println("# " + date.toString() + " # " + text );
+	}
+	
+	// Adds two numbers
+	public float addition(float number1, float number2) {
+		float result = number1 + number2;
+		this.print("Result od addition is " + result);
+		this.addToHistory(result);
+		return result;
+	}
+	
+	// Subtracts two numbers
+	public float substraction(float number1, float number2) {
+		float result = number1 - number2;
+		this.print("Result od substraction is " + result);
+		this.addToHistory(result);
+		return result;
+	}
+	
+	
+	// Multiplies two numbers
+	public float multiplication(float number1, float number2) {
+		float result = number1 * number2;
+		this.print("Result od multiplication is " + result);
+		this.addToHistory(result);
+		return result;
+	}
+	
+	// Divides two numbers
+	public float division(float number1, float number2) {
+		float result = number1 / number2;
+		this.print("Result od division is " + result);
+		this.addToHistory(result);
+		return result;
+	}
+	
+	// Adds result to history array
+	private void addToHistory(float item) {
+		this.history.add(item);
+	}
+	
+	// Print the whole history
+	public void printHistory() {
+		for (float result: this.history) {
+			this.print(String.valueOf(result));
+		}
+	}
+	
+	// Compare two numbers
+	public void compareNumbers(float number1, float number2) {
+		if (number1 > number2) {
+			this.print(number1 + " is bigger than " + number2);
+		} else if (number2 > number1) {
+			this.print(number1 + " is smaller than " + number2);
+		} else {
+			this.print(number1 + " is the same as " + number2);
+		}
+	}
+	
+	// Adds results to history
+	public void loadHistoryFromFile(String fileName) {
+		try {
+			File historyFile = new File(fileName);
+			BufferedReader reader = new BufferedReader(new FileReader(historyFile));
+            String readLine = "";
+            while ((readLine = reader.readLine()) != null) {
+                this.addToHistory(Float.parseFloat(readLine));
+            }
+            reader.close();
+            this.print("History form file was loaded!");
+		} catch (Exception e) {
+			this.print("File could not be loaded! Because:" + e);
+		}
+	}
+}
